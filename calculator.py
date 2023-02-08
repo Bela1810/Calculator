4549745be2e5d6a678df351bb72ad922a7955ba9
PERSONAL <danielgomezaristizabal2020@gmail.com>
2023-02-07 23:55:16 -0500

    Menu_calculadora

--- calculator.py
+++ calculator.py
@@ -17,6 +17,36 @@ class Menu_Calculadora(Operaciones):
     opcion = input("Elige opcion:  ")
     return opcion
 
+  while True:
+      opcion = menu_calculadora(Numero1, Numero2)
+      Numero1 = float(input('Introduce numero: '))
+      Numero2 = float(input('Introduce numero: '))
+
+      if opcion == '1':
+          print("Calculadora de Suma")
+          resultado = Sumar(Numero1, Numero2)
+      elif opcion == '2':
+          print("Calculadora de Resta")
+          resultado = Restar(Numero1, Numero2)
+      elif opcion == '3':
+          print("Calculadora de Dividir")
+          resultado = Dividir(Numero1, Numero2)
+      elif opcion == '4':
+          print("Calculadora de Multiplicar")
+          resultado = Multiplicar(Numero1, Numero2)
+      elif opcion == '5':
+          print("Calculadora de Potenciacion")
+          resultado = Potenciacion(Numero1, Numero2)
+
+      print("Resultado: ", resultado)
+
+      Result = resultado
+
+      seguir = input('Seguir operando? si/no')
+      if seguir == "no":
+        break
+
+
 
 
 
