e4f2d7bc5a6a806311f54109fbe5b68ed76a9b62
belac <belacaste3@gmail.com>
2023-02-07 23:02:21 -0500

    Initial suma commit

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
-def Sumar(self, Num1, Num2):
-    return Num1 + Num2
 
 from abc import ABC
 
 class Operaciones:
 
-  def Multiplicar(self, Num1, Num2):
+def Sumar(self, Num1, Num2):
+    return Num1 + Num2
+
+def Multiplicar(self, Num1, Num2):
     return Num1 * Num2
 
 
