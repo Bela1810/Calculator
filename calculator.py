bf671684d18b292dce8e29e888b4c0efd7e93a96
belac <belacaste3@gmail.com>
2023-02-07 23:16:01 -0500

    Initial commit

--- calculator.py
+++ calculator.py
@@ -7,6 +7,45 @@ def Sumar(self, Num1, Num2):
 def Multiplicar(self, Num1, Num2):
     return Num1 * Num2
 
+class Menu_Calculadora(Operaciones):
+  def menu_calculadora():
+    print("1. Sumar")
+    print("2. Restar")
+    print("3. Dividir")
+    print("4. Multiplicar")
+    print("5. Potenciacion")
+    opcion = input("Elige opcion:  ")
+    return opcion
+
+  while True:
+    opcion = menu_calculadora()
+    Numero1 = float(input('Introduce numero: '))
+    Numero2 = float(input('Introduce numero: '))
+
+    if opcion == '1':
+      print("Calculadora de Suma")
+      resultado = Sumar(Numero1, Numero2)
+    elif opcion == '2':
+      print("Calculadora de Resta")
+      resultado = Restar(Numero1, Numero2)
+    elif opcion == '3':
+      print("Calculadora de Dividir")
+      resultado = Dividir(Numero1, Numero2)
+    elif opcion == '4':
+      print("Calculadora de Multiplicar")
+      resultado = Multiplicar(Numero1, Numero2)
+    elif opcion == '5':
+      print("Calculadora de Potenciacion")
+      resultado = Potenciacion(Numero1, Numero2)
+
+    print("Resultado: ", resultado)
+
+    N1 = resultado
+
+    seguir = input('Seguir operando? si/no')
+    if seguir == "no":
+      break
+
 
 
 
